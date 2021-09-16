c98ab67a8c4dcb5920b26a1866f2ee9cf3ecc2d2
fdujwc <fdujwc.cn@gmail.com>
2021-09-16 19:30:54 +0800

    bugfix

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 # 程序启动脚本
 pip3 install -r requirements.txt
-nohup python3 ./src/main.py > ./log.txt 2>&1 &
+export WORK_DIR=/root/CryptoQuant
+export PYTHONPATH="${PYTHONPATH}:$WORK_DIR
+nohup python3 $WORK_DIR/src/main.py > ./log.txt 2>&1 &
