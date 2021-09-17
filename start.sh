9cfe385b94534695421346896bc3551f5f74f2f3
fdujwc <fdujwc.cn@gmail.com>
2021-09-17 10:51:49 +0800

    bugfix

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
+# /bin/bash
 # 程序启动脚本
 WORK_DIR=`pwd`
-logfile=$WORK_DIR/log/log_start_sh.txt
+LOG_DIR=$WORK_DIR/log
+mkdir $LOG_DIR
+logfile=$LOG_DIR/log_start_sh.txt
 cd ~ && source .bashrc && cd $WORK_DIR
 export PYTHONPATH="${PYTHONPATH}:$WORK_DIR"
 nohup pip3 install -r $WORK_DIR/requirements.txt > $logfile 2>&1 &
