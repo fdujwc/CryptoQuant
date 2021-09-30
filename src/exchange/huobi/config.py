403273bf902942485ddbc95c4c44c97c12ba91ae
fdujwc <fdujwc.cn@gmail.com>
2021-09-30 14:49:15 +0800

    test

--- src/exchange/huobi/config.py
+++ src/exchange/huobi/config.py
@@ -5,14 +5,20 @@ from os import environ
 
 
 class Secret(object):
-    # 读取操作access key，用于发送读取请求
-    READ_ACCESS = environ["READ_ACCESS"]
-    # 读取操作secret，用于验证交易所返回报文的签名
-    READ_SECRET = environ["READ_SECRET"]
-    # 交易操作access key，用于发送交易请求
-    TRADE_ACCESS = environ["TRADE_ACCESS"]
-    # 交易操作secret，用于验证交易所返回报文的签名
-    TRADE_SECRET = environ["TRADE_SECRET"]
+    try:
+        # 读取操作access key，用于发送读取请求
+        READ_ACCESS = environ["READ_ACCESS"]
+        # 读取操作secret，用于验证交易所返回报文的签名
+        READ_SECRET = environ["READ_SECRET"]
+        # 交易操作access key，用于发送交易请求
+        TRADE_ACCESS = environ["TRADE_ACCESS"]
+        # 交易操作secret，用于验证交易所返回报文的签名
+        TRADE_SECRET = environ["TRADE_SECRET"]
+    except:
+        READ_ACCESS = 'environ["READ_ACCESS"]'
+        READ_SECRET = 'environ["READ_SECRET"]'
+        TRADE_ACCESS = 'environ["TRADE_ACCESS"]'
+        TRADE_SECRET = 'environ["TRADE_SECRET"]'
     # 签名算法
     SIGN_METHOD = "HmacSHA256"
     # 签名算法版本
